2eee733604ecb1d0f6ab97376208778343409115
thammo4 <thammo4@uic.edu>
2024-05-06 22:46:33 -0400

    Remove extraneous comment from Account source file

--- src/Account.cpp
+++ src/Account.cpp
@@ -7,14 +7,6 @@ Account::Account(const std::string& accountNumber, const std::string& authToken,
 
 }
 
-
-// void Account::fetchBalances() const {
-// 	std::string endpoint = BALANCES_ENDPOINT;
-// 	endpoint.replace(endpoint.find("{account_id}"), std::string("{account_id}").length(), accountNumber);
-// 	nlohmann::json balances = sendGetRequest(endpoint);
-// 	std::cout << "BALANCES\n" << balances.dump(4) << std::endl;
-// }
-
 void Account::fetchBalances() const {
 	std::string endpoint = BALANCES_ENDPOINT;
 	size_t pos = endpoint.find("{account_id}");
